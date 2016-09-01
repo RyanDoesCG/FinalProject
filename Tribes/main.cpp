2db7174ea1f80363cd02f33fa659544adf734ca8
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2016-09-01 07:17:09 +0100

    Initial Commit

---
+++ Tribes/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tribes
+//
+//  Created by ryan needham on 01/09/2016.
+//  Copyright © 2016 Dissertation. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
