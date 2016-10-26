63471d9bbdffa1361a60b82adbb44081b21aeb88
MyForteIsTimeTravel <RyanNeedham@outlook.com>
2016-10-26 17:54:50 +0100

    Game class missed from last commit. Added now.

--- Tribes/Source/Game.cpp
+++ Tribes/Source/Game.cpp
@@ -10,56 +10,51 @@
 #include "../Headers/GLFW/glfw3.h"
 
 Game::Game() {
-    seed = generateSeed();
-    std::cout << "World Seed: " << seed << std::endl << std::endl;
-    srand(seed);
+    srand(generateSeed());
     
     // put engine stuff together
     if (BUILD_MODE == CINEMATIC) {
-        window   = new Window   (1920, 1400, "tribes - cinematic");
-        backdrop = new Backdrop (40000);
-        hud      = new HUD      (this, 1920, 1080, &delta);
+        windowWidth  = 1920;
+        windowHeight = 1080;
     }
     
     else if (BUILD_MODE == DEVELOPMENT) {
-        window   = new Window   (848, 480, "tribes - debug");
-        backdrop = new Backdrop (20000);
-        hud      = new HUD      (this, 848, 480, &delta);
+        windowWidth  = 1200;
+        windowHeight = 648;
     }
     
-    planet   = new Planet (TOUGHER, seed);
+    if (initGLFW()) {std::cout << "SDL initialisation failure. Exiting\n";  exit(1);}
+    if (initGLEW()) {std::cout << "GLEW initialisation failure. Exiting\n"; exit(1);}
+        
+    backdrop = new Backdrop     (40000);
+    hud      = new HUD          (this,  &delta);
+    planet   = new Planet       (TOUGHER, seed);
     input    = new InputHandler (this);
     state    = RUNNING;
 }
 
 Game::~Game() {
-    free(window);
-    free(input);
-    free(planet);
+
 }
 
 void Game::begin() {
+    glClearColor (0.1f, 0.1f, 0.1f, 0.0f);
 
-    // fucks up on vsync and just spins mad CPU - FIX THIS
-    while (!window->finished()) {
-        float start = glfwGetTime();
-        input->update();
-        
+    while (!glfwWindowShouldClose(window)) {
         if (state == RUNNING) {
-            window->clear();
+            glClear (GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
             
-            backdrop->update(); // draw stars
-            planet->update();   // draw planet
-            hud->update();      // draw hud
+            input     ->update(); // poll input
+            backdrop->update();   // draw stars
+            planet->update();     // draw planet
+            hud->update();        // draw hud
             
-            window->update();
+            glfwSwapBuffers(window);
         }
         
         else {
             glfwWaitEvents();
         }
-
-        calculateFPS(start);
     }
 }
 
@@ -69,21 +64,51 @@ void Game::end() {
 
 // toggle pause on and off
 void Game::pause() {
-    (state == RUNNING) ?
-        state = PAUSED
-    :
-        state = RUNNING;
-}
-
-void Game::calculateFPS (float start) {
-  //  BROKEN
-   //float smoothing = 0.7;
-   //delta = (delta * smoothing) + ((glfwGetTime() - start) * 1000 * (1.0-smoothing));
-
-   delta = (glfwGetTime() - start) * 1000;
+    (state == RUNNING) ? state = PAUSED : state = RUNNING;
 }
 
 long Game::generateSeed() {
     srand(static_cast<unsigned int>(time(0)));
-    return rand();
+    int s = rand();
+    std::cout << "World Seed: " << s << std::endl <<std::endl;
+    return s;
+}
+
+int Game::initGLFW () {
+    // start glfw Subsystem
+    if (glfwInit() == GLFW_FALSE) return 1;
+    
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);                  // OpenGL Version
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);                  // OpenGL Version
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // OpenGL Profile
+    glfwWindowHint(GLFW_RESIZABLE, GL_TRUE);                        // Resizable Window
+    glfwWindowHint(GLFW_DOUBLEBUFFER, GL_TRUE);                     // Double Buffering
+//  glfwWindowHint(GLFW_MAXIMIZED, GL_TRUE);                        // Stick to corner
+//  glfwWindowHint(GLFW_SAMPLES, 4);                                // Multisampling
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // macOS requires this
+    glfwSwapInterval(1);                                            // enable VSYNC
+    
+    window = glfwCreateWindow(windowWidth, windowHeight, "Tribes - OpenGL", nullptr, nullptr);
+    if (!window) return 1;
+    
+    glfwMakeContextCurrent(window);
+    
+    return 0;
+}
+
+int Game::initGLEW () {
+    // initialise GLEW
+    GLenum error = glewInit();
+    if (error) return 1;
+    
+    // viewport
+    glfwGetFramebufferSize (window, &windowWidth, &windowHeight);
+    glViewport (0, 0, windowWidth, windowHeight);
+    
+    glEnable    (GL_DEPTH_TEST);    // Z BUFFERING
+//  glEnable    (GL_MULTISAMPLE);   // MULTISAMPLING
+    glEnable    (GL_BLEND);         // TEXT BLENDING
+    glBlendFunc (GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    
+    return 0;
 }
